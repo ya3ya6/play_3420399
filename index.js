9cf40b78497312583d3254b6544381dc942768fd
ya3ya6 <ya3ya63@gmail.com>
2024-04-10 07:05:52 +0330

    fsfs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('.'));
 app.get('/', function(req, res){
-   res.send('hey');
+   res.send('<h1>wow</h1>');
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT || 3000, () => {
